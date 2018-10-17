1e88ddc461dde57f2eacd0bf1378840689729e28
Chris McCormick <chris@mccormick.cx>
2018-10-17 14:06:26 +0800

    Remove pbr based setup and replace.
    The pbr based setup.py blews up on Python 2.7.3 for us in production.

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
-import setuptools
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+
+from distutils.core import setup
+
+setup(
+    version="0.1",
+    name='pyhiccup',
+    description='Pyhiccup fork without pbr',
+    url='https://github.com/CultureCounts/pyhiccup',
+    packages=['pyhiccup'],
+)
+
